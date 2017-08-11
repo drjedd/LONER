ad2e46ff3ef3ff72935c52cef828e7bb74a47d6b
Jean-Loup Didelot <jl.didelot@yahoo.com>
2017-08-11 12:06:56 +0100

    Scene switch gates experimentations
    Trying multiple colliders on terrain plane itself, reverted to child
    gameobject boxes for now

--- Assets/Scripts/TerrainBehaviour.cs
+++ Assets/Scripts/TerrainBehaviour.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Collider2D))]
-public class GateBehaviour : MonoBehaviour {
+public class TerrainBehaviour : MonoBehaviour
+{
+	/* TEST CLASS, WILL PROBABLY NOT BE USED
+	 * (also needs to require a collider)
+	 */
+
 
 	public bool gateIsOpen = true;
 	public int destinationSceneIndex;
 
-	private Vector2 playerRelativePosition;
-	private Bounds gateBounds;
-
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		if (destinationSceneIndex < 0 || destinationSceneIndex > SceneManager.sceneCountInBuildSettings)
 		{
 			//scene is invalid: make the gate a wall instead by disabling "Trigger" mode on Collider
@@ -31,26 +33,16 @@ public class GateBehaviour : MonoBehaviour {
 				GetComponent<Collider2D>().isTrigger = false;
 			}
 		}
-
-		//get gate bounds to calculate player relative position
-		gateBounds = GetComponent<Renderer>().bounds;
-		
 	}
 
 	void OnTriggerEnter2D(Collider2D collider)
-	{
-		
-		if (collider.gameObject == Player.Instance.gameObject) {
+	{	
 
-			//currently does not work. The idea is to get the player position relative to the gate so we can use that in the next scene
-			playerRelativePosition = new Vector2(Mathf.InverseLerp(gateBounds.min.x, gateBounds.max.x, collider.transform.position.x), Mathf.InverseLerp(gateBounds.min.y, gateBounds.max.y, collider.transform.position.y));
-
-			Debug.Log(playerRelativePosition);
-
+		if (collider.gameObject == Player.Instance.gameObject)
+		{
 			Debug.Log("LOADING SCENE #" + destinationSceneIndex);
 			SceneManager.LoadScene(destinationSceneIndex);
 
-			//debug: player (and camera) position "reset"
 			Player.Instance.transform.position = new Vector3(3.5f, 3, 0);
 		}
 	}
